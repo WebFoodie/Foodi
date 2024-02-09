f23d4da5ab1ce570e2bdc7160512adcd9f3302ab
Prabhjot0786 <prabhjot6280472@gmail.com>
2024-02-09 22:35:58 +0530

    changes
    changes

--- foodi-client/src/pages/home/Home.jsx
+++ foodi-client/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Banner from '../../components/Banner'
-
+import Categories from './Categories'
+import SpecialDishes from './SpecialDishes'
+import Testimonials from './Testimonials'
 const Home = () => {
   return (
     <div>
-      <Banner/>
+      <Banner />
+      <Categories />
+      <SpecialDishes />
+      <Testimonials />
     </div>
-  )
-}
+  );
+};
 
 export default Home
---
+++ foodi-client/src/pages/home/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className='section-container'>
+      <div className = 'flex flex-col md:flex-row items-center justify-between gap-12'>
+           <div className = 'md:w-1/2'></div>
+         </div>
+      </div>
+  )
+}
+export default Testimonials
