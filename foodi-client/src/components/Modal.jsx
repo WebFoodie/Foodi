fa983f5ea22eb2a02d449b938c699a43275e77f6
Prabhjot0786 <prabhjot6280472@gmail.com>
2024-02-11 23:14:07 +0530

    adding login page

--- foodi-client/src/components/Modal.jsx
+++ foodi-client/src/components/Modal.jsx
@@ -1,19 +1,87 @@
-import react from 'react'
-
+import react from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { useForm } from "react-hook-form"
 const Modal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit= (data) => console.log(data)
   return (
-    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+    <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click the button below to close</p>
-        <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn">Close</button>
+        <div className="modal-action flex flex-col justify-center mt-0">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="card-body"
+            method="dialog"
+          >
+            <h3 className="font-bold text-lg">Please Login!</h3>
+
+            {/* email */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                {...register("email")}
+              />
+            </div>
+            {/* Password */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                {...register("password")}
+              />
+              <label className="label mt-1">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            {/* error */}
+            {/* login  btn */}
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                value="Login"
+                className="btn bg-green text-white"
+              />
+            </div>
+            <p className="text-center my-2">
+              Donot have an account?
+              <Link to="/signup" className="underline text-red ml-1">
+                Signup now
+              </Link>
+            </p>
           </form>
+          {/* social sign in */}
+          <div className="text-center space-x-3 mb-5">
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGoogle />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaFacebookF />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGithub />
+            </button>
+          </div>
         </div>
       </div>
-    </dialog> 
-  )
-}
+    </dialog>
+  );
+};
 export default Modal;
